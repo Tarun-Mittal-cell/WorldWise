455611b85ff510df2ff1602d213654436895c9b2
Tarun Mittal <tarunmittal@Taruns-iMac.local>
2024-01-24 22:32:31 -0500

    added nested routes and then also used NavLinks to update the URLS directly and also have tabs instead of using useState

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,14 +2,15 @@ import AppNav from "./AppNav";
 import Logo from "./Logo";
 import styles from "./Sidebar.module.css";
 import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
-      <div className={styles.spacer}></div> {/* Spacer element */}
+      <Outlet />
+      <div className={styles.spacer}></div>
       <Footer />
     </div>
   );
